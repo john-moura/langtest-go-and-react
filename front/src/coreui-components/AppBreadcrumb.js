1732093c33738a2ee189417257abfe0f43ae7656
John Moura <johnmoura.88@gmail.com>
2025-12-01 02:23:18 +0100

    Fix breadcrumb

--- front/src/coreui-components/AppBreadcrumb.js
+++ front/src/coreui-components/AppBreadcrumb.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, matchPath } from 'react-router-dom'
 
 import routes from '../coreui-routes'
 
@@ -9,7 +9,14 @@ const AppBreadcrumb = () => {
   const currentLocation = useLocation().pathname
 
   const getRouteName = (pathname, routes) => {
-    const currentRoute = routes.find((route) => route.path === pathname)
+    const currentRoute = routes.find((route) => {
+      // Check for exact match first
+      if (route.path === pathname) return true;
+
+      // Check for dynamic route match
+      const match = matchPath({ path: route.path, end: true }, pathname);
+      return match !== null;
+    })
     return currentRoute ? currentRoute.name : false
   }
 
